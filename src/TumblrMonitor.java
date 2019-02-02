09111453db7590ae5559ea55cd8e85775e4dd064
Peter Amerkhanian <peteamer@gmail.com>
2019-02-02 13:19:41 -0500

    put some of the post data into a Hashmap so I can more easily use them later, moved info filtering for post into a method

--- src/TumblrMonitor.java
+++ src/TumblrMonitor.java
@@ -1,8 +1,11 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import java.io.IOException;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import java.io.IOException;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.Scanner;
@@ -12,6 +15,7 @@ import java.util.Scanner;
  * their respective images from the first page of a given Tumblr profile.
  */
 public class TumblrMonitor {
+
     /**
      * Get's a Tumblr URL from user input
      * @return - a string of a URL
@@ -32,8 +36,12 @@ public class TumblrMonitor {
             Document doc = Jsoup.connect(url).get();
             return doc.select("div#entry");
         } catch (Exception IOException) {
-            System.out.println("Invalid URL, please try again (Don't forget to add 'http://' to the beginning");
-             return get_html(get_input());
+            if (!url.startsWith("http://")) {
+                return get_html("http://" + url);
+            } else {
+                System.out.println("Invalid URL, please try again");
+                return get_html(get_input());
+            }
         }
     }
 
@@ -54,24 +62,45 @@ public class TumblrMonitor {
         return match;
     }
 
+    /**
+     * Creates a HashMap object of a given entry's date, post text and post url
+     * @param entry an element containing the html of a single tumblr post
+     * @return a HashMap that contains three keys, date, text and url,
+     * and their respective pieces of info
+     */
+    private static HashMap info_printer(Element entry) {
+        HashMap<String, String> map = new HashMap<>();
+        String date = entry.select("div.permalink span.permalink1").text();
+        System.out.println("date: " + filter_date(date));
+        map.put("date", date);
+        String post_text = entry.select("p").text();
+        System.out.println("text: " + post_text);
+        map.put("text", post_text);
+        String post_link = entry.select("a[href]").first().attr("href");
+        System.out.println("url: " + post_link);
+        map.put("url", post_link);
+        return map;
+    }
+
     public static void main(String[] args) throws IOException {
         System.out.println();
         Elements entries = get_html(get_input());
         for (Element entry: entries) {
-            String date = entry.select("div.permalink span.permalink1").text();
-            System.out.println("date: " + filter_date(date));
-            String post_link = entry.select("a[href]").first().attr("href");
-            System.out.println("url: " + post_link);
+            HashMap map = info_printer(entry);
             if (entry.select("img").eachAttr("src").isEmpty()) {
-                Document post_doc = Jsoup.connect(post_link).get();
+                Document post_doc = Jsoup.connect(map.get("url").toString()).get();
+                int count = 0;
                 for (Element image: post_doc.select("meta[content$=.jpg]")) {
+                    count ++;
                     String img_url = image.attr("content");
-                    System.out.println("pics: " + img_url);
+                    System.out.println("pic " + count + ": " + img_url);
                 }
             }
+            int count = 0;
             for (Element image: entry.select("img")) {
+                count ++;
                 String img_url = image.attr("src");
-                System.out.println("pics: " + img_url);
+                System.out.println("pic " + count + ": " + img_url);
                 }
             System.out.println();
         }
